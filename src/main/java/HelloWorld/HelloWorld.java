c86ae3da0e31f545b2741b0829b9ee7ef47f5294
Роман <phillroman24@gmail.com>
2018-07-27 13:24:53 +0300

    Решенное первое задание.

--- src/main/java/HelloWorld/HelloWorld.java
+++ src/main/java/HelloWorld/HelloWorld.java
@@ -2,17 +2,21 @@ package HelloWorld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        MySystem.MyOutput.myOut.myPrintln();
-    }
-    public static class MySystem{
-        public static class MyOutput{
-             static MyOutput myOut = myPrintln();
-            public static void myPrintln(String arg){
-                System.out.println("Hello world");
-            }
-        }
 
+        MySystem.myOut.myPrintln("Hello World");
+    }
+
+    public static class MySystem {
+        static MyOutput myOut = new MyOutput();
+    }
+
+    public static class MyOutput {
+        public static void myPrintln(String phrase) {
+            System.out.println(phrase);
+        }
     }
 }
+
+
 
 
