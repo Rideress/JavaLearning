7531638a045df3edc97ae4b1c6c533162e5f8b9b
Роман <phillroman24@gmail.com>
2018-08-16 15:47:21 +0300

    Задание 7.(конструктор)

--- src/main/java/TaskSeven/TaskSeven.java
+++ src/main/java/TaskSeven/TaskSeven.java
@@ -2,14 +2,16 @@ package TaskSeven;
 
 public class TaskSeven {
     public static void main(String[] args) {
+        SystemUnit mySystemUnit = new SystemUnit();
+        mySystemUnit.turnOn();
 
 
     }
 
-     abstract  class Devices {
+     abstract  class Device {
         protected String name;
 
-        public Devices(String name){
+        public Device(String name){
             this.name = name;
         }
         public  String getName() {
@@ -30,48 +32,29 @@ public class TaskSeven {
     public  class Computer {
     }
 
-    public  class SystemUnit {
-        public  void turnOn(){
+    public static class SystemUnit {
+        CPU myCpu = new CPU("Процессор ");
+        public void turnOn(){
+            myCpu.turnOn();
+            myCpu.getName();
+
+
         }
-         class CPU extends Devices {
+         class CPU extends Device {
+
              public CPU(String name) {
                  super(name);
              }
-             Devices myCpu = new Devices("Процессор "){
-                 @Override
-                 public String getName() {
-                     return super.getName();
-                 }
-
-                 @Override
-                 public void turnOn() {
-                     super.turnOn();
-                 }
-
-                 @Override
-                 public void turnOff() {
-                     super.turnOff();
-                 }
-             };
          }
-         class memory extends Devices{
-            Devices meMemory = new Devices("Запоминающее устройство ") {
-                @Override
-                public void turnOn() {
-                    super.turnOn();
-                }
 
-                @Override
-                public void turnOff() {
-                    super.turnOff();
-                }
-            };
 
-             public memory(String name) {
+         }
+          class Memory extends Device {
+             public Memory(String name) {
                  super(name);
              }
          }
-    }
+
 
 
      public static class OutputDevices {
